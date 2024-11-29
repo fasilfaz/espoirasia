e60c8b99a2ed6d33e0d320b4bb4147f516e93860
Fasilfaz <fazsby2001@gmail.com>
2024-11-29 16:30:05 +0530

    new

--- script.js
+++
@@ -1,7 +0,0 @@
-const buttons = document.querySelector('.buttons');
-const navList = document.querySelector('.navbar .menu-list');
-
-buttons.addEventListener('click', () => {
-    buttons.classList.toggle('active');
-    navList.classList.toggle('active');
-});
